86c2366f40d3285f36016955f10ea68548dc8c87
chirag gaur <chiragwalker@gmail.com>
2023-04-05 23:01:59 +0530

    6 question pending

--- block-BJaabj/code/index.js
+++ block-BJaabj/code/index.js
@@ -2,6 +2,11 @@
 1. Create a function named `sayHello` that alerts a message `Hello World!`.
 */
 
+// function sayHello () {
+//    alert ("Hello World");
+// }
+//  sayHello ();
+
 /*
 2. Create a function named `fullName` with the following steps:
 
@@ -11,6 +16,15 @@
   - Alert `fullName`
 */
 
+// function fullName () {
+//   let firstName = prompt(" Enter your first name ");
+//   // alert (firstName);
+//   let lastName = prompt (" Enter your last name ");
+//   // alert (lastName);
+//   let fullName = firstName +" " +lastName ;
+//   alert (fullName); 
+// }
+//   fullName ();
 /*
 3. Create a function named `addTwoNumbers` with the following steps:
 
@@ -19,6 +33,15 @@
   - Add the value of firstNum and secondNum and store it in another variable named `sum`.
   - Alert `sum`
 */
+
+// function addTwoNumbers () {
+//     let firstNum = +prompt(" Enter the first number ");  
+//     alert (firstNum);
+//     let secondNum = +prompt (" Enter the second number "); 
+//     alert (secondNum);
+//     let sum = firstNum + secondNum ;
+//     alert (sum);
+// } addTwoNumbers ();
 
 /*
 4. Create a function named `getTable` with the following steps:
@@ -38,13 +61,29 @@
   9 * 9 = 81
   9 * 10 = 90
 */
+// let num = +prompt ("Enter a number");
+// let i = 1;
+// function getTable () {
+//    while (i <= 10){
+//     console.log (`${num} * ${i} = ${num * i}`);
+//     i++;
+//    }
 
+// } getTable ();
 /*
 5. Create a function named `isLeapYear` with the following steps:
 
   - Prompts a message saying `Enter a year` and store the value into variable named`year`
   - Alert `[year] is a leap year` otherwise `[year] is not a leap year`
 */
+// function isLeapYear () {
+//   let year = +prompt (" Enter a year ");
+//   if (year % 4 == 0 ) {
+//     alert (" is a leap year ");
+//   } else {
+//     alert (" is not a leap year ");
+//   }
+// } isLeapYear ();
 
 /*
 6. Create a function named `getFactorial` with the following steps:
